3276d5848379c9143ee6ba8a48a5ea566fb175a1
Givaldo Almeida dos Santos <givasaju@gmail.com>
2025-07-05 15:01:45 -0300

    Update appst.py

--- appst.py
+++ appst.py
@@ -5,6 +5,7 @@ def main():
     st.subheader("Givaldo Almeida - AluraDevops Google 2025!")
     st.subheader("Givaldo Almeida - AluraDevops Google 2025!")
     st.subheader("Givaldo Almeida - AluraDevops Google 2025!")
+    st.subheader("Testando com novas alterações para o Google Cloud!")
     
 if __name__ == "__main__":
     main()
